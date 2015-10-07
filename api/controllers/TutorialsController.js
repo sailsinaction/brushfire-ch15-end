7ae97973c75e7010679e5874737b15ac997780f0
Irl Nathan <irlnathan@gmail.com>
2015-10-07 12:58:24 -0500

    updated search action to have properly formatted date

--- api/controllers/TutorialsController.js
+++ api/controllers/TutorialsController.js
@@ -13,75 +13,103 @@ module.exports = {
       title: 'Sed ut perspiciatis unde omnis',
       description: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea.',
       owner: 'sailsinaction',
-      createdOn: '2015-09-27T16:32:55.000Z',
+      id: 1,
+      createdAt: '2015-09-27T16:32:55.000Z',
+      updatedAt: '2015-10-07T14:57:12.000Z',
       totalTime: '3h 22m',
       stars: '4'
     }, {
       title: 'Sed ut perspiciatis unde omnis',
       description: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea.',
       owner: 'sailsinaction',
-      createdOn: '2015-09-27T16:32:55.000Z',
+      createdAt: '2015-09-27T16:32:55.000Z',
       totalTime: '3h 22m',
       stars: '5'
     }, {
       title: 'Sed ut perspiciatis unde omnis',
       description: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea.',
       owner: 'sailsinaction',
-      createdOn: '2015-09-27T16:32:55.000Z',
+      id: 2,
+      createdAt: '2015-09-27T16:32:55.000Z',
+      updatedAt: '2015-10-07T14:57:12.000Z',
       totalTime: '3h 22m',
       stars: '3'
     }, {
       title: 'Sed ut perspiciatis unde omnis',
       description: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea.',
       owner: 'sailsinaction',
-      createdOn: '2015-09-27T16:32:55.000Z',
+      id: 3,
+      createdAt: '2015-09-27T16:32:55.000Z',
+      updatedAt: '2015-10-07T14:57:12.000Z',
       totalTime: '3h 22m',
       stars: '5'
     }, {
       title: 'Sed ut perspiciatis unde omnis',
       description: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea.',
       owner: 'sailsinaction',
-      createdOn: '2015-09-27T16:32:55.000Z',
+      id: 4,
+      createdAt: '2015-09-27T16:32:55.000Z',
+      updatedAt: '2015-10-07T14:57:12.000Z',
       totalTime: '3h 22m',
       stars: '1'
     }, {
       title: 'Sed ut perspiciatis unde omnis',
       description: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea.',
       owner: 'sailsinaction',
-      createdOn: '2015-09-27T16:32:55.000Z',
+      id: 5,
+      createdAt: '2015-09-27T16:32:55.000Z',
+      updatedAt: '2015-10-07T14:57:12.000Z',
       totalTime: '3h 22m',
       stars: '5'
     }, {
       title: 'Sed ut perspiciatis unde omnis',
       description: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea.',
       owner: 'sailsinaction',
-      createdOn: '2015-09-27T16:32:55.000Z',
+      id: 6,
+      createdAt: '2015-09-27T16:32:55.000Z',
+      updatedAt: '2015-10-07T14:57:12.000Z',
       totalTime: '3h 22m',
       stars: '2'
     }, {
       title: 'Sed ut perspiciatis unde omnis',
       description: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea.',
       owner: 'sailsinaction',
-      createdOn: '2015-09-27T16:32:55.000Z',
+      id: 7,
+      createdAt: '2015-09-27T16:32:55.000Z',
+      updatedAt: '2015-10-07T14:57:12.000Z',
       totalTime: '3h 22m',
       stars: '4'
     }, {
       title: 'Sed ut perspiciatis unde omnis',
       description: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea.',
       owner: 'sailsinaction',
-      createdOn: '2015-09-27T16:32:55.000Z',
+      id: 8,
+      createdAt: '2015-09-27T16:32:55.000Z',
+      updatedAt: '2015-10-07T14:57:12.000Z',
       totalTime: '3h 22m',
       stars: '5'
     }];
 
-    return res.json({tutorials: tutorials});
+    // Format the date the Tutorial was created into time ago (e.g. 10 days ago)
+    var Datetime = require('machinepack-datetime');
+
+    var updatedTutorials = _.map(tutorials, function(tutorial){
+      
+      var niceTimeAgoString = Datetime.timeFrom({
+        toWhen: Datetime.parse({
+          datetime: tutorial.createdAt
+        }).execSync(),
+        fromWhen: new Date().getTime()
+      }).execSync();
+
+      tutorial.createdAt = niceTimeAgoString;
+      return tutorial;
+    });
+
+    return res.json({tutorials: updatedTutorials});
   },
 
   rateTutorial: function(req, res) {
-
-    console.log(req.param('rating'));
-    console.log(req.param('id'));
-
 
     return res.json({
       rating: req.param('rating'),
