3f3593e6e71c05e2eedfac7e18c7e42c990dd937
Irl Nathan <irlnathan@gmail.com>
2015-10-21 14:26:32 -0500

    changed layout to false from null

--- api/responses/forbidden.js
+++ api/responses/forbidden.js
@@ -48,12 +48,12 @@ module.exports = function forbidden (data, options) {
   // Otherwise try to guess an appropriate view, or if that doesn't
   // work, just send JSON.
   if (options.view) {
-    return res.view(options.view, { data: data, layout: null });
+    return res.view(options.view, { data: data, layout: false });
   }
 
   // If no second argument provided, try to serve the default view,
   // but fall back to sending JSON(P) if any errors occur.
-  else return res.view('403', { data: data, layout: null }, function (err, html) {
+  else return res.view('403', { data: data, layout: false }, function (err, html) {
 
     // If a view error occured, fall back to JSON(P).
     if (err) {
