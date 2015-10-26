73336ec633a3e8b46d601299863f0928c051ecae
Irl Nathan <irlnathan@gmail.com>
2015-10-26 17:53:06 -0500

    adding tutorialId from url

--- assets/js/controllers/showVideoPageController.js
+++ assets/js/controllers/showVideoPageController.js
@@ -5,7 +5,10 @@ angular.module('brushfire').controller('showVideoPageController', ['$scope', '$h
     loading: false
   };
 
-$scope.me = window.SAILS_LOCALS.me;
+  // Get the tutorial id form the current URL path:  /tutorials/55/videos/edit
+  $scope.fromUrlTutorialId = window.location.pathname.split('/')[2];
+
+  $scope.me = window.SAILS_LOCALS.me;
 
   
 }]);
