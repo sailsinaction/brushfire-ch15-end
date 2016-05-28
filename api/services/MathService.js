62f70842bcf37969a0b6fd2b99a8565bc305b3ad
Mike McNeil <mikermcneil@users.noreply.github.com>
2016-05-28 17:54:03 -0500

    Add documentation to MathService.

--- api/services/MathService.js
+++ api/services/MathService.js
@@ -1,5 +1,29 @@
 module.exports = {
   
+  /**
+   * `MathService.calculateAverage()`
+   * 
+   * Compute the average star rating for a set of Brushfyre ratings.
+   *
+   * -------------------------------------------------------------------------------------------------------
+   * @required {Array} ratings
+   *    e.g.
+   *    ```
+   *    [
+   *      {
+   *        // stars: 4,
+   *        // ...any other props...
+   *      },
+   *      // ...
+   *    ]
+   *    ```
+   * 
+   * @returns {Number}  [the average star rating]
+   *   e.g. 3.5
+   * 
+   * @throws {Error} if an empty array of ratings is provided.
+   * -------------------------------------------------------------------------------------------------------
+   */
   calculateAverage: function (options) {
 
     var sumTutorialRatings = 0;
